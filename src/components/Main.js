b959a511c469bb8cdc6d20422cc4c37e89cda3c9
jeffk77 <jeff.kushner@me.com>
2019-03-13 00:06:14 -0400

    Layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Main = () => (
   <div>
     <main>
-      MAIN PAGE
+      <div className="row">
+        <div className="col s7 push-s5">
+          <div className="headPic z-depth-3">
+          </div>
+        </div>
+        <div className="col s5 pull-s5">
+          WELCOME TO THE BTTA
+        </div>
+      </div>
     </main>
   </div>
 );
